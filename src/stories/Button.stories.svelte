a7349bc90bc992f7930f91a7bdc5a07f69c6b4c3
Khashayar Toodehfallah <khashayar@toodehfallah.com>
2021-08-14 22:02:19 -0400

    Some button updates, dark mode okay

--- src/stories/Button.stories.svelte
+++ src/stories/Button.stories.svelte
@@ -1,19 +1,24 @@
 <script>
   import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import { null_to_empty } from "svelte/internal";
   import Button from "./Button.svelte";
 </script>
 
-<Meta title="Button" component={Button} />
+<Meta title="Inputs/Button" component={Button} />
 
 <Template let:args>
   <Button {...args} on:click={args.onClick} />
 </Template>
 
 <Story
-  name="Secondary Small"
+  name="Singleton"
   args={{
-    label: "Secondary Small",
+    label: "Button",
     primary: false,
     large: false,
+    hasIcon: false,
+    iconRight: false,
+    iconLeft: false,
+    group: false,
   }}
 />
--- src/stories/Button.svelte
+++ src/stories/Button.svelte
@@ -1,5 +1,10 @@
 <script>
   import { createEventDispatcher } from "svelte";
+  import {
+    ChevronLeftIcon,
+    ChevronRightIcon,
+    HomeIcon,
+  } from "svelte-feather-icons";
   /**
    * Is this the principal call to action on the page?
    */
@@ -11,32 +16,107 @@
   export let large = true;
 
   /**
+   * Is there an icon?
+   */
+
+  export let hasIcon = false;
+
+  /**
+   * Is there a right icon?
+   */
+
+  export let iconRight = true;
+
+  /**
+   * Is there a left icon?
+   */
+
+  export let iconLeft = true;
+
+  /**
    * Button contents
    */
   export let label = "";
 
+  export let group = false;
+
   const dispatch = createEventDispatcher();
 
   /**
-   * Optional click handler
+   * Click handler
    */
-  function onClick(event) {
-    dispatch("click", event);
+  function buttonClick(event) {
+    dispatch("button-click", event);
+  }
+  function iconRightClick(event) {
+    dispatch("icon-right-click", event);
+  }
+  function iconLeftClick(event) {
+    dispatch("icon-left-click", event);
   }
 
-  $: sizeClasses = large ? "p-5 text-base font-semibold" : "px-2 py-1 text-sm";
+  let commonClasses =
+    "p-2 inline-flex items-center border rounded-md text-center font-semibold";
+
+  $: sizeClasses = large ? "p-3" : "p-2";
+
+  $: iconColorClasses =
+    (hasIcon || iconRight || iconLeft) && primary
+      ? "text-white"
+      : (hasIcon || iconRight || iconLeft) && !primary
+      ? "dark:text-gray-300 text-blue-700"
+      : "";
+
+  $: middleIconClasses =
+    hasIcon && (iconRight || iconLeft) ? "mx-2 flex-none" : "";
+
+  $: sideIconsClasses =
+    iconRight || iconLeft
+      ? "first:pr-2 last:pl-2 flex flex-col items-center"
+      : "";
+
+  $: iconSize = large ? "1.5x" : "1x";
+
   $: typeClasses = primary
-    ? "dark:bg-yellow-600 bg-blue-500 text-white"
-    : "dark:bg-red-600 dark:text-white bg-white text-blue-500 border border-blue-500 border-solid";
+    ? "justify-between dark:bg-blue-600 dark:border-blue-600 bg-blue-700 border-blue-700 text-white hover:bg-blue-600 active:bg-blue-800"
+    : "justify-start border-gray-200 border-solid dark:border-gray-700 dark:text-gray-300 text-blue-700 dark:bg-gray-900 bg-white hover:bg-blue-200 active:bg-blue-300 hover:border-blue-200 active:border-blue-300";
+
+  $: baseClasses = group ? "" : "";
+
+  $: labelClasses = large
+    ? "m-0 text-base py-2 px-3 flex-grow flex-shrink-0"
+    : "m-0 text-xs tracking-wide flex-grow flex-shrink-0";
 </script>
 
 <button
   type="button"
-  class="underline {sizeClasses} {typeClasses}"
-  on:click={onClick}
+  class="{commonClasses} {baseClasses} {typeClasses} {sizeClasses}"
+  on:click={buttonClick}
 >
-  {label}
-</button>
+  {#if iconLeft}
+    <button type="button" class={sideIconsClasses} on:click={iconLeftClick}>
+      <ChevronLeftIcon
+        class="stroke-current {iconColorClasses}"
+        size={iconSize}
+      />
+    </button>
+  {/if}
 
-<style>
-</style>
+  {#if hasIcon}
+    <HomeIcon
+      class="stroke-current {iconColorClasses} {middleIconClasses}"
+      size={iconSize}
+    />
+  {:else}
+    <p class={labelClasses}>{label}</p>
+  {/if}
+
+  {#if iconRight}
+    <button type="button" class={sideIconsClasses} on:click={iconRightClick}>
+      <ChevronRightIcon
+        class="stroke-current {iconColorClasses}"
+        size={iconSize}
+      />
+    </button>
+  {/if}
+</button>
